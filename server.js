e77b874642434f630e34e03e86c44d45e405b683
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-08-08 02:39:40 +0200

    create the Express server:
    it should listen on the port set by the environment variable PORT or by default 5000
    it should load all routes from the file routes/index.js
    Inside the folder routes, create a file index.js that contains all endpoints of our API:
    
    GET /status => AppController.getStatus
    GET /stats => AppController.getStats
    Inside the folder controllers, create a file AppController.js that contains the definition of the 2 endpoints:
    
    GET /status should return if Redis is alive and if the DB is alive too by using the 2 utils created previously: { redis: true, db: true } with a status code 200
    GET /stats should return the number of users and files in DB: { users: 12, files: 1231 } with a status code 200
    users collection must be used for counting all users
    files collection must be used for counting all files

--- server.js
+++ server.js
@@ -1,43 +1,9 @@
 import express from 'express';
-import injectRoutes from './routes';
+import startServer from './libs/boot';
+import injectRoutes from './routes/index';
+import injectMiddlewares from './libs/middlewares';
 
 const server = express();
-
-/**
- * Loads the appropriate environment variables for an event.
- */
-const envLoader = () => {
-  const env = process.env.npm_lifecycle_event || 'dev';
-  const path = env.includes('test') || env.includes('cover') ? '.env.test' : '.env';
-
-  if (existsSync(path)) {
-    const data = readFileSync(path, 'utf-8').trim().split('\n');
-
-    for (const line of data) {
-      const delimPosition = line.indexOf('=');
-      const variable = line.substring(0, delimPosition);
-      const value = line.substring(delimPosition + 1);
-      process.env[variable] = value;
-    }
-  }
-};
-
-const startServer = (api) => {
-  envLoader();
-  const port = process.env.PORT || 5000;
-  const env = process.env.npm_lifecycle_event || 'dev';
-  api.listen(port, () => {
-    console.log(`[${env}] API has started listening at port:${port}`);
-  });
-};
-
-/**
- * Adds middlewares to the given express application.
- * @param {express.Express} api The express application.
- */
-const injectMiddlewares = (api) => {
-  api.use(express.json({ limit: '200mb' }));
-};
 
 injectMiddlewares(server);
 injectRoutes(server);
